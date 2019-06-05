ff9472bacfc91de0bf87903884636a8f0ccc2c38
Max Silver <Maxbsilver@gmail.com>
2019-06-04 20:09:06 -0600

    display cosmetics

--- src/containers/cosmetics/CosmeticsView.jsx
+++ src/containers/cosmetics/CosmeticsView.jsx
@@ -1,36 +1,59 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getAllCosmeticsThunk } from "../../api/cosmetics";
+import CosmeticItemContainer from "../../components/cosmeticItemContainer/CosmeticItemContainer";
 
 export class CosmeticsView extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      filter: "All",
-      allCosmetics: [],
-      gliders: [],
-      outfits: [],
-      pickaxes: []
+      filter: "all",
+      displayItems: []
     };
   }
-  selectDisplay = () => {
+  async componentDidMount() {
+    if (this.props.cosmeticItems.length === 0)
+      await this.props.getAllCosmeticsThunk();
+    this.setState({ displayItems: this.props.cosmeticItems });
+  }
 
+  selectDisplay = filter => {
+    const { cosmeticItems } = this.props;
+    const displayItems = cosmeticItems.filter(
+      cosmetic => cosmetic.item.type === filter
+    );
+    this.setState({ displayItems, filter });
   };
 
-  handleClick = () => {};
-  async componentDidMount() {
-    if (this.props.cosmeticItems === 0) await this.props.getAllCosmeticsThunk();
-  }
+  handleClick = e => {
+    const { name } = e.target;
+    this.selectDisplay(name);
+  };
 
   render() {
     return (
       <section className="cosmetics-container">
-        <div className="control-container">
-          <button onClick={this.handleClick}>Gliders</button>
-          <button onClick={this.handleClick}>Pickaxes</button>
-          <button onClick={this.handleClick}>Outfits</button>
-        </div>
+        {!this.props.loading && (
+          <React.Fragment>
+            <h2>Cosmetics</h2>
+            <div className="control-container">
+              <button className="gliders-btn" name="glider" onClick={this.handleClick}>
+                Gliders
+              </button>
+              <button  className="pickaxes-btn" name="pickaxe" onClick={this.handleClick}>
+                Pickaxes
+              </button>
+              <button   className="outfits-btn" name="outfit" onClick={this.handleClick}>
+                Outfits
+              </button>
+            </div>
+            <section className="cosmetic-item-container">
+              <CosmeticItemContainer
+                displayItems={this.state.displayItems}
+              />
+            </section>
+          </React.Fragment>
+        )}
       </section>
     );
   }
